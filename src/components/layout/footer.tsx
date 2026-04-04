2723c2dd42b743b8665a5bb12a26de84ce2ff767
Codyk12 <cody.kesler@icloud.com>
2026-04-03 18:34:03 -0700

    updated footer.tsx

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -6,9 +6,6 @@ export function Footer() {
   return (
     <footer className='flex items-center justify-between border-t px-4 py-6'>
       <p className='text-sm'>© {currentYear} C4G. All rights reserved.</p>
-      <Link href='/team' className='text-sm'>
-        Team
-      </Link>
     </footer>
   );
 }
